62aedd0493567edb5af015004676856ceeff2313
w1lltj <cycorn8@gmail.com>
2019-08-09 16:09:55 +0800

    init

---
+++ pass-data-between-screens/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pass-data-between-screens
+//
+//  Created by Will on 9/8/19.
+//  Copyright © 2019 Will. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
